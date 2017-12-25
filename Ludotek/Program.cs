150b93649540900f514c545469c5ff449e7ee903
nbeschu <nicolas.beschu@gmail.com>
2017-12-25 17:20:02 +0100

    Passage en Angular 5 + correction namespace

--- Ludotek/Program.cs
+++ Ludotek/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Ludothèque
+namespace Ludotek
 {
     public class Program
     {
