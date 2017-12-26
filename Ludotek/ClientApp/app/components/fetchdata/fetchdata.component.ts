e2916b4f3de8d6aafca1aae2a6d14e1490e95689
nbeschu <nicolas.beschu@gmail.com>
2017-12-26 22:05:16 +0100

    suppression composant Counter + maj routes vers API REST + maj route 'search' + ajout recherche par tag + ajout recherche par tag et nom d'item

--- Ludotek/ClientApp/app/components/fetchdata/fetchdata.component.ts
+++ Ludotek/ClientApp/app/components/fetchdata/fetchdata.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Item } from '../../models/item.model';
 import { Observable } from 'rxjs/Observable';
 import { FetchDataService } from '../../services/fetchdata.service';
+import { Tag } from '../../models/tag.model';
 
 @Component({
     selector: 'fetchdata',
@@ -9,13 +10,36 @@ import { FetchDataService } from '../../services/fetchdata.service';
 })
 export class FetchDataComponent implements OnInit {
     items: Observable<Array<Item>>;
+    existingTags: Observable<Array<Tag>>;
+    selectedTag: string
 
-    constructor(private ludotheque: FetchDataService) { };
-
-    ngOnInit() {
+    constructor(private ludotheque: FetchDataService) {
+        this.selectedTag = void 0;
     };
 
+    ngOnInit() {
+        this.existingTags = this.ludotheque.getExistingTags();
+    };
+
+    onSelectionChange(tag: Tag) {
+        this.selectedTag = tag.nomTag;
+        console.log(this.selectedTag);
+    }
+
     getLudotheque(searchedItem: string) {
-        this.items = this.ludotheque.getLudotheque(searchedItem);
+        if (this.selectedTag === undefined) {
+            this.getLudothequeByItem(searchedItem);
+        }
+        else {
+            this.getLudothequeByTagAndItem(this.selectedTag, searchedItem);
+        }
+    };
+
+    getLudothequeByItem(searchedItem: string) {
+        this.items = this.ludotheque.getLudothequeByItem(searchedItem);
+    };
+
+    getLudothequeByTagAndItem(searchedTag: string, searchedItem: string) {
+        this.items = this.ludotheque.getLudothequeByTagAndItem(searchedTag, searchedItem);
     };
 }
--- Ludotek/ClientApp/app/services/fetchdata.service.ts
+++ Ludotek/ClientApp/app/services/fetchdata.service.ts
@@ -1,6 +1,7 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Item } from '../models/item.model';
+import { Tag } from '../models/tag.model';
 import { deserialize } from 'json-typescript-mapper';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -9,8 +10,18 @@ import 'rxjs/add/operator/map';
 export class FetchDataService {
     constructor(private http: HttpClient) {}
 
-    getLudotheque(searchedItem: string): Observable<Array<Item>> {
-        return this.http.get('api/Ludotheque/' + searchedItem)
+    getExistingTags(): Observable<Array<Tag>> {
+        return this.http.get('api/tag/')
+            .map((result: Array<Object>) => result.map((item: Object) => deserialize(Tag, item)));
+    }
+
+    getLudothequeByItem(searchedItem: string): Observable<Array<Item>> {
+        return this.http.get('api/ludotheque/' + searchedItem)
+            .map((result: Array<Object>) => result.map((item: Object) => deserialize(Item, item)));
+    }
+
+    getLudothequeByTagAndItem(searchedTag: string, searchedItem: string): Observable<Array<Item>> {
+        return this.http.get('api/tag/' + searchedTag + '/items/' + searchedItem)
             .map((result: Array<Object>) => result.map((item: Object) => deserialize(Item, item)));
     }
 }
